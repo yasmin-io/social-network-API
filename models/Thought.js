f65823dc1c50f481f511ffb24236af956bda0717
Yasmin <yasmin.bunnie@gmail.com>
2022-05-09 23:51:39 +0100

    Created thought model

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema } = require("mongoose");
+const moment = require("moment");
+// Create a Reaction Schema to import
+
+const thoughtSchema = new Schema(
+  {
+    thoughText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAt) => moment(createdAt).format("YYYY-MM-DD HH:mm"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
